0c4e47032f0b0b047f624bbc50f107b3620d9389
MasterSkepticista <kbshah1998@outlook.com>
2024-07-20 11:53:18 +0530

    Add optimizer

--- train.py
+++ train.py
@@ -3,12 +3,13 @@ import functools
 import os
 
 import jax
+from flax import jax_utils
 import jax.numpy as jnp
 import optax
 import tensorflow as tf
 import tiktoken
 from model import GPT, get_config, load_hf_pretrained
-from utils import compute_flops
+import utils as u
 
 if os.environ.get("OMPI_COMM_WORLD_SIZE", -1) != -1:
   jax.distributed.initialize()
@@ -35,17 +36,23 @@ def build_pipeline(data_dir: str,
   ds = ds.repeat()
   ds = ds.shuffle(10_000)
 
-  def _preprocess(proto):
+  def _decode(proto):
     ex = tf.io.parse_single_example(proto, {"tokens": tf.io.FixedLenFeature([], tf.string)})
     return tf.io.decode_raw(ex["tokens"], tf.uint16)
 
-  ds = ds.map(_preprocess, num_parallel_calls=tf.data.AUTOTUNE)
+  ds = ds.map(_decode, num_parallel_calls=tf.data.AUTOTUNE)
   ds = ds.unbatch()  # Flattens to a stream of tokens.
   ds = ds.batch(block_size + 1, drop_remainder=True)
   ds = ds.map(lambda x: (x[:-1], x[1:]))  # Input and Next (completion) pairs.
   ds = ds.batch(batch_size, drop_remainder=True)
 
   ds = ds.prefetch(tf.data.AUTOTUNE)
+
+  # Shard batches to GPUs.
+  ds = iter(ds)
+  ds = map(u.tf_to_numpy, ds)
+  ds = map(u.shard_batches, ds)
+  ds = jax_utils.prefetch_to_device(ds, 2)
   return ds
 
 # @jax.jit
@@ -73,7 +80,7 @@ def main():
   def init(rng):
     dummy_input = jnp.ones((1, cfg.block_size), dtype=jnp.int32)
     params = jax.jit(model.init, backend="cpu")(rng, dummy_input)["params"]
-    gflops = compute_flops(
+    gflops = u.compute_flops(
         functools.partial(model.apply, {"params": params}), [dummy_input]) / 1e9
     return params, gflops
 
@@ -101,8 +108,27 @@ def main():
       info(tokenizer.decode(next_token[0]), end="")
 
   # Build data pipeline.
-  train_ds = build_pipeline("data", batch_size=32, block_size=cfg.block_size, train=True)
-  val_ds = build_pipeline("data", batch_size=32, block_size=cfg.block_size, train=False)
+  train_iter = build_pipeline("data", batch_size=32, block_size=cfg.block_size, train=True)
+  val_iter = build_pipeline("data", batch_size=32, block_size=cfg.block_size, train=False)
+
+  # Build optimizer and train state.
+  max_lr = 6e-4
+  min_lr = 0.1 * max_lr
+  max_steps = 19073
+  warmup_steps = 715
+  sched_fn = u.get_cosine_lr_schedule(max_lr, min_lr, max_steps, warmup_steps)
+
+  tx = optax.chain(
+    optax.clip_by_global_norm(1.0),
+    optax.adamw(
+      learning_rate=sched_fn, 
+      b1=0.9, 
+      b2=0.95,
+      weight_decay=0.1,
+      mask=jax.tree.map(lambda p: p.ndim > 1, params)),
+  )
+  opt_state = tx.init(params)
+
 
 if __name__ == "__main__":
   main()
