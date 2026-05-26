e92c15efe6b15c63ce67163529f32ffefbc2e90f
MasterSkepticista <karanshah1698@gmail.com>
2026-05-26 13:29:32 +0530

    new mfu microbenchmark

--- benchmark.py
+++ benchmark.py
@@ -1,66 +1,84 @@
-"""Flash Attention Pallas Benchmark."""
+"""Self-attention microbenchmark."""
+import time
 from absl import app
+from absl import flags
+from functools import partial
 
 import jax
 import jax.numpy as jnp
-from jax.experimental import pallas as pl
 
-from attention import naive_attention, cudnn_attention, flash_attention
+from attention import flash_attention, naive_attention, cudnn_attention
 
-def online_softmax(x: jax.Array):
-  out = jnp.zeros_like(x).astype(jnp.float32)
-  TILE_SIZE = 3
+FLAGS = flags.FLAGS
 
-  m, s = -jnp.inf, 0
-  for i in range(x.shape[0] // TILE_SIZE):
-    idx = pl.dslice(i * TILE_SIZE, TILE_SIZE)
-    x_tile = x[idx]
-    
-    m_i = jnp.max(x_tile)
-    m_prev = m
-    m = jnp.maximum(m, m_i)
-    s = s * jnp.exp(m_prev - m) + jnp.sum(jnp.exp(x_tile - m))
-  
-  for i in range(x.shape[0] // TILE_SIZE):
-    idx = pl.dslice(i * TILE_SIZE, TILE_SIZE)
-    x_tile = x[idx]
-    out = out.at[idx].set(jnp.exp(x_tile - m) / s)
+flags.DEFINE_string("impl", "pallas", 
+  "Attention implementation to benchmark: 'pallas', 'naive', or 'cudnn'.")
+flags.DEFINE_integer("head_dim", 64, 
+  "Dimension of each attention head (64 or 128 recommended).")
 
-  return out
+MAX_TOKENS = 16 * 1024
+
+def generate_tensors(B, T, H, C):
+  keys = jax.random.split(jax.random.PRNGKey(42), 3)
+  q = jax.random.normal(keys[0], (B, T, H, C), dtype=jnp.bfloat16)
+  k = jax.random.normal(keys[1], (B, T, H, C), dtype=jnp.bfloat16)
+  v = jax.random.normal(keys[2], (B, T, H, C), dtype=jnp.bfloat16)
+  return q, k, v
+
+def timeit(fn, *args, num_iters=10):
+  # Warmup
+  for _ in range(2):
+    out = fn(*args)
+  jax.block_until_ready(out)
+
+  start_time = time.monotonic()
+  for _ in range(num_iters):
+    out = fn(*args)
+  jax.block_until_ready(out)
+  end_time = time.monotonic()
+
+  return (end_time - start_time) / num_iters
 
 def main(argv):
-  B, T, H, C = 1, 1024, 12, 64
-  keys = jax.random.split(jax.random.key(42), 4)
+  match(FLAGS.impl):
+    case "pallas":
+      attention_fn = flash_attention
+    case "naive":
+      attention_fn = naive_attention
+    case "cudnn":
+      attention_fn = cudnn_attention
+    case _:
+      raise ValueError(f"Invalid implementation: {FLAGS.impl}")
 
-  # FA cuDNN works on f16 and bf16 only.
-  q = jax.random.normal(keys[0], (B, T, H, C), jnp.bfloat16)
-  k = jax.random.normal(keys[1], (B, T, H, C), jnp.bfloat16)
-  v = jax.random.normal(keys[2], (B, T, H, C), jnp.bfloat16)
-  do = jax.random.normal(keys[3], (B, T, H, C), jnp.bfloat16)
+  q, k, v = generate_tensors(1, 1024, 12, 64)
+  o_ref = naive_attention(q, k, v, causal=False)    
+  jit_fwd_fn = jax.jit(partial(attention_fn, causal=False))
+  jnp.allclose(jit_fwd_fn(q, k, v), o_ref, rtol=1e-2, atol=1e-2)
+  print("Results match. Starting benchmark...")
+  print("Benchmarking attention implementation:", FLAGS.impl)
 
-  # Forward pass
-  o_ref = naive_attention(q, k, v, causal=True)
-  o_flash = flash_attention(q, k, v, causal=True)
-  print("Forward pass result match:", jnp.allclose(o_ref, o_flash, atol=1e-2, rtol=1e-2))
+  for T in [1024, 2048, 4096, 8192, 16384]:
+    bs = MAX_TOKENS // T
+    num_heads = 2048 // FLAGS.head_dim
+    q, k, v = generate_tensors(bs, T, num_heads, FLAGS.head_dim)
 
-  # Backward pass
-  def loss_ref(q, k, v):
-    return jnp.sum(naive_attention(q, k, v, causal=True) * do)
-  dq_ref, dk_ref, dv_ref = jax.grad(loss_ref, argnums=(0, 1, 2))(q, k, v)
-  print("Reference shapes:", dq_ref.shape, dk_ref.shape, dv_ref.shape)
+    # Forward pass
+    jit_fwd_fn = jax.jit(partial(attention_fn, causal=False))
+    flop_count = 4 * T**2 * FLAGS.head_dim * num_heads * bs
+    avg_time = timeit(jit_fwd_fn, q, k, v)
+    tflops = flop_count * 1e-12 / avg_time
+    mfu = (tflops / 121) * 100  # Using 121 TFLOPs for L4 as per notebook context
+    print(f"(fwd) T={T:5d}, B={bs:3d}, TFLOP/s={tflops:.2f}, MFU={mfu:.2f}%")
 
-  def loss(q, k, v):
-    return jnp.sum(flash_attention(q, k, v, causal=True) * do)
-  dq_flash, dk_flash, dv_flash = jax.grad(loss, argnums=(0, 1, 2))(q, k, v)
-  print("Flash shapes:", dq_flash.shape, dk_flash.shape, dv_flash.shape)
-
-  print("Backward pass dQ match:", jnp.allclose(dq_ref, dq_flash, atol=1e-2, rtol=1e-2))
-  print("Backward pass dK match:", jnp.allclose(dk_ref, dk_flash, atol=1e-2, rtol=1e-2))
-  print("Backward pass dV match:", jnp.allclose(dv_ref, dv_flash, atol=1e-2, rtol=1e-2))
-
-
-
-
+    # Forward + Backward pass
+    def loss(q, k, v):
+      out = attention_fn(q, k, v, causal=False)
+      return jnp.sum(out)
+    jit_fwd_bwd_fn = jax.jit(jax.grad(loss, argnums=(0, 1, 2)))
+    avg_time = timeit(jit_fwd_bwd_fn, q, k, v)
+    tflops = flop_count * 3.5 * 1e-12 / avg_time  # Backward is ~2.5x forward
+    mfu = (tflops / 121) * 100  # Using 121 TFLOPs for L4 as per notebook context
+    print(f"(fwd+bwd) T={T:5d}, B={bs:3d}, TFLOP/s={tflops:.2f}, MFU={mfu:.2f}%")
 
 
 if __name__ == "__main__":
